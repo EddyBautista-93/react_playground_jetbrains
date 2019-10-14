a94f7c8e77e51a4beda932359070f84d4f706d6a
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-10-14 12:11:14 -0500

    added incr with effect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+const App = () => {
+
+  const APP_ID = '0b743333';
+  const APP_KEY = '9a8747a5b4221a830e36f6d282126831';
+  const exampleReq =
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+     console.log('Effect has been run')
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Hello React</h1>
+          <form className="search-form">
+              <input className="search-bar" type="text"/>
+              <button className="search-button" type="submit">Search</button>
+          </form>
+          <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      </div>
   );
-}
+};
 
 export default App;
